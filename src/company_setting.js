f844ea032b89a067f8a52514625fbfe90fa81e1c
akshit <akshit.kanani.contractor@eqonex.com>
2022-08-08 18:49:48 +0530

    company setting haf

--- src/company_setting.js
+++ src/company_setting.js
@@ -10,6 +10,9 @@ import ellipse80 from "./images/Ellipse80.png";
 import vector80 from "./images/Vector80.png";
 import line from "./images/Line 10.png";
 import group166 from "./images/Group 166.png";
+import SettingsIcon from "@material-ui/icons/Settings";
+import line30 from "./images/Line 36.png";
+import vector81 from "./images/Vector81.png";
 
 const CompanySetting = () => {
   return (
@@ -93,15 +96,21 @@ const CompanySetting = () => {
 
         {/* middal content start */}
         <div className="middalmain">
-          <div style={{ lineHeight: "0px", float: "left", fontSize: "19px",textAlignLast: "start"  }}>
-            <h4>General</h4>
+          <div
+            style={{
+              lineHeight: "0px",
+              float: "left",
+              fontSize: "19px",
+            }}
+          >
+            <h3>General</h3>
           </div>
           <div>
             <img src={line} alt="" style={{ width: "100%" }} />
           </div>
           <div style={{ float: "left", fontSize: "18px" }}>
-            <h5 style={{ textAlignLast: "start" }}>Comapny Name</h5>
-            <p style={{ textAlignLast: "start",fontSize: "14px" }}>
+            <h3>Comapny Name</h3>
+            <p style={{ fontSize: "14px" }}>
               Keep it Short And Don't include Things Like "inc"."LLC" Etc{" "}
             </p>
             {/* <input type="Type Here" placeholder="Type Here"/> */}
@@ -115,12 +124,128 @@ const CompanySetting = () => {
           </div>
 
           <div style={{ float: "left", fontSize: "18px" }}>
-            <h5 style={{ textAlignLast: "start" }}>Comapny Location</h5>
-            <p  style={{ textAlignLast: "start",width:"567px" }}>
+            <h3>Comapny Location</h3>
+            <p style={{ width: "567px", fontSize: "14px" }}>
               Select The Main Location Of Your Company. Breezy Will Use This To
               Designate Your Primary Timezone For System Emails.
             </p>
+            <div>
+              <img
+                src={group166}
+                alt=""
+                style={{ width: "422px", height: "40px", float: "left" }}
+              />
+            </div>
           </div>
+
+          <div>
+            <h3>Single sign-On(SSO)</h3>
+            <p>Require Employees To Sign in With Your Company's SSO System.</p>
+            <div className="buttonhead">
+              <div>
+                <p>Google Apps</p>
+              </div>
+              <div>
+                <button type="button" className="buttonStyle">
+                  <SettingsIcon />
+                  Setting{" "}
+                </button>
+              </div>
+            </div>
+            <div>
+              <img
+                src={line30}
+                alt=""
+                style={{ border: " 0.4px solid #C4C4C4", width: "95%" }}
+              />
+            </div>
+
+            <div className="buttonhead">
+              <div>
+                <p>Okta</p>
+              </div>
+              <div>
+                <button type="button" className="buttonStyle">
+                  <SettingsIcon />
+                  Setting{" "}
+                </button>
+              </div>
+            </div>
+            <div>
+              <img
+                src={line30}
+                alt=""
+                style={{ border: " 0.4px solid #C4C4C4", width: "95%" }}
+              />
+            </div>
+
+            <div className="buttonhead">
+              <div>
+                <p>OneLogin</p>
+              </div>
+              <div>
+                <button type="button" className="buttonStyle">
+                  <SettingsIcon />
+                  Setting{" "}
+                </button>
+              </div>
+            </div>
+            <div>
+              <img
+                src={line30}
+                alt=""
+                style={{ border: " 0.4px solid #C4C4C4", width: "95%" }}
+              />
+            </div>
+
+            <div className="buttonhead">
+              <div>
+                <p>SAML</p>
+              </div>
+              <div>
+                <button type="button" className="buttonStyle">
+                  <SettingsIcon />
+                  Setting{" "}
+                </button>
+              </div>
+            </div>
+            <div>
+              <img
+                src={line30}
+                alt=""
+                style={{ border: " 0.4px solid #C4C4C4", width: "95%" }}
+              />
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <h3>Employee Auto-Join</h3>
+              <p>
+                Let Your Employees Use Their Company Email Address To
+                Automagically Add Themselves As Members.
+              </p>
+              <p>Employee Auto-Join</p>
+              <p>
+                Auto-Join URL :
+                <img src={vector81} alt="" style={{ paddingLeft: "10px" }} />
+                <a href="" style={{ color: "#4CABCD" }}>
+                  http://app.breezy.hr/signup/zenwe
+                </a>{" "}
+              </p>
+            </div>
+            <div>
+              <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" className="buttonJoin1">
+                  Enabled
+                </button>
+                <button type="button" className="buttonJoin2">
+                  Disabled
+                </button>
+              </div>
+            </div>
+          </div>
+
         </div>
 
         {/* middal content stop */}
